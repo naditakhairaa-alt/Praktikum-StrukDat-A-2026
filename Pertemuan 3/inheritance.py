6f509e1cf642ce1b10123cb5f2c9d632e100d38a
naditakhairaa-alt <naditakhairaa@gmail.com>
2026-02-27 21:04:08 +0700

    Praktikum Pertemuan 3

---
+++ Pertemuan 3/inheritance.py
@@ -0,0 +1,53 @@
+#inheritance code
+class Person: #parent class
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):  #child class
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname) #super function
+
+x = Student("Martin", "Edward")
+x.printname()
+
+#add properties
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Martin", "Edward", 2019)
+print(x.graduationyear)
+
+#add method
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Martin", "Edward", 2024)
+x.welcome()
+
---
+++ Pertemuan 3/innerClass.py
@@ -0,0 +1,47 @@
+class Outer:
+  def __init__(self):
+    self.name = "Outer Class"
+
+  class Inner:
+    def __init__(self):
+      self.name = "Inner Class"
+
+    def display(self):
+      print("This is the inner class")
+
+outer = Outer()
+print(outer.name)
+
+#access
+class Outer:
+  def __init__(self):
+    self.name = "Outer"
+
+  class Inner:
+    def __init__(self):
+      self.name = "Inner"
+
+    def display(self):
+      print("Hello from inner class")
+
+outer = Outer()
+inner = outer.Inner()
+inner.display
+
+#multiple
+class Computer:
+  def __init__(self):
+    self.cpu = self.CPU()
+    self.ram = self.RAM()
+
+  class CPU:
+    def process(self):
+      print("Processing data...")
+
+  class RAM:
+    def store(self):
+      print("Storing data...")
+
+computer = Computer()
+computer.cpu.process()
+computer.ram.store()
